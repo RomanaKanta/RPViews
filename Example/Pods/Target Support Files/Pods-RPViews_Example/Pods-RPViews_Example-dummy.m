a99d9587fc791dea329aad78b7c89b8c73a2362f
Romana <romana@mislbd.com>
2021-01-21 11:31:32 +0600

    create new project

---
+++ Example/Pods/Target Support Files/Pods-RPViews_Example/Pods-RPViews_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RPViews_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RPViews_Example
+@end
---
+++ Example/Pods/Target Support Files/RPViews/RPViews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RPViews : NSObject
+@end
+@implementation PodsDummy_RPViews
+@end
